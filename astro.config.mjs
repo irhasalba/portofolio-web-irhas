0ca5eeae23ae08d78346b497847154cd64d150f1
irhasa <irhasalba19@gmail.com>
2024-09-23 20:21:20 +0700

    rollback domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:'https://irhasalba.my.id',
+  site:'https://irhasalba.github.io',
   base: '/portofolio-web-irhas/',
 });
