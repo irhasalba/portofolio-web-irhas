9c897715f4f1117cd22cadb1f1592820538ac356
irhasa <irhasalba19@gmail.com>
2024-09-23 09:38:44 +0700

    enhancement base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://irhasalba.github.io',
-  base: 'portofolio-web-irhas',
+  base: '/',
 });
