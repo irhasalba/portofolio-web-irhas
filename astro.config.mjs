de93900625fc34b0c40a1168b33aa89d0366fdf2
irhasa <irhasalba19@gmail.com>
2024-09-23 20:05:37 +0700

    add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:'https://irhasalba.github.io',
+  site:'https://irhasalba.my.id',
   base: '/portofolio-web-irhas/',
 });
